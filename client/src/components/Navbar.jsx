9acd25fa97ed3e112fffb312ec57863877980c5b
Amin Ghs <amin_ghs@yahoo.com>
2022-01-04 13:21:21 +0100

    Installing react-icon and ethers then start initialize NavbarItem component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,10 +3,23 @@ import { AiOutlineClose } from 'react-icons/ai';
 
 import logo from '../../images/logo.png';
 
+const NavbarItem = ({ title, classProps}) => {
+    return (
+        <li className={`mx-4 cursor-pointer ${classProps}`}>
+            {title}
+        </li>
+    );
+}
+
 const Navbar = () => {
     return (
         <nav className="w-full flex md:justify-center">
-
+            <div className='md:flex-[0.5] flex-initial justify-center items-center'>
+                <img src={logo} alt="logo" className='w-32 cursor-pointer' />
+            </div>
+            <ul className='text-white md:flex hiddent list-none flex-row justify-between items-center flex-initial'>
+                <NavbarItem />
+            </ul>
         </nav>
     );
 }
